c60f20a96c12cab004f3dfe5b0bc10e8ef4548fe
Håkan Edling <hakan@tidyui.com>
2019-12-11 08:42:23 +0100

    Updated to SR 1

--- blog/razor/Pages/Archive.cshtml.cs
+++ blog/razor/Pages/Archive.cshtml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Piranha;
+using Piranha.AspNetCore.Services;
 using RazorBlog.Models;
 
 namespace RazorBlog.Pages
@@ -8,18 +10,44 @@ namespace RazorBlog.Pages
     public class ArchiveModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
     {
         private readonly IApi _api;
+        private readonly IModelLoader _loader;
+
         public BlogArchive Data { get; private set; }
 
-        public ArchiveModel(IApi api) : base()
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="api"></param>
+        /// <param name="loader"></param>
+        public ArchiveModel(IApi api, IModelLoader loader) : base()
         {
             _api = api;
+            _loader = loader;;
         }
 
-        public async Task OnGet(Guid id, int? year = null, int? month = null, int? page = null,
-            Guid? category = null, Guid? tag = null)
+        /// <summary>
+        /// Gets the model data.
+        /// </summary>
+        /// <param name="id">The page id</param>
+        /// <param name="year">The optional year</param>
+        /// <param name="month">The optional month</param>
+        /// <param name="pagenum">The optional page number</param>
+        /// <param name="category">The optional category</param>
+        /// <param name="tag">The optional draft</param>
+        /// <param name="draft">If the draft should be fetched</param>
+        /// <returns>The result</returns>
+        public async Task<IActionResult> OnGet(Guid id, int? year = null, int? month = null, int? pagenum = null,
+            Guid? category = null, Guid? tag = null, bool draft = false)
         {
-            Data = await _api.Pages.GetByIdAsync<Models.BlogArchive>(id);
-            Data.Archive = await _api.Archives.GetByIdAsync(id, page, category, tag, year, month);
+            Data = await _loader.GetPage<Models.BlogArchive>(id, HttpContext.User, draft);
+
+            if (Data != null)
+            {
+                Data.Archive = await _api.Archives.GetByIdAsync(id, pagenum, category, tag, year, month);
+
+                return Page();
+            }
+            return NotFound();
         }
     }
 }
