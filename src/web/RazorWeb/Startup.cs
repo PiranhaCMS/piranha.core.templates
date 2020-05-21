453b58473f08ca50e523d5520a2ad8e016529c38
Håkan Edling <hakan@tidyui.com>
2020-05-21 08:44:42 +0200

    New wip web template

--- src/web/RazorWeb/Startup.cs
+++ src/web/RazorWeb/Startup.cs
@@ -1,39 +1,100 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Piranha;
+using Piranha.AttributeBuilder;
+#if (UseSQLServer)
+using Piranha.AspNetCore.Identity.SQLServer;
+using Piranha.Data.EF.SQLServer;
+#elif (UseMySql)
+using Piranha.AspNetCore.Identity.MySQL;
+using Piranha.Data.EF.MySql;
+#elif (UsePostgreSql)
+using Piranha.AspNetCore.Identity.PostgreSQL;
+using Piranha.Data.EF.PostgreSql;
+#else
+using Piranha.AspNetCore.Identity.SQLite;
+using Piranha.Data.EF.SQLite;
+#endif
+using Piranha.Manager.Editor;
 
 namespace RazorWeb
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="configuration">The current configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Service setup
+            services.AddPiranha(options =>
+            {
+                options.AddRazorRuntimeCompilation = true;
+
+#if (UseBlobStorage)
+                options.UseBlobStorage(_config.GetConnectionString("blobstorage"));
+#else
+                options.UseFileStorage();
+#endif
+                options.UseImageSharp();
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseMemoryCache();
+#if (UseSQLServer)
+                options.UseEF<SQLServerDb>(db =>
+                    db.UseSqlServer(_config.GetConnectionString("piranha")));
+                options.UseIdentityWithSeed<IdentitySQLServerDb>(db =>
+                    db.UseSqlServer(_config.GetConnectionString("piranha")));
+#elif (UseMySql)
+#elif (UsePostgreSql)
+#else
+                options.UseEF<SQLiteDb>(db =>
+                    db.UseSqlite(_config.GetConnectionString("piranha")));
+                options.UseIdentityWithSeed<IdentitySQLiteDb>(db =>
+                    db.UseSqlite(_config.GetConnectionString("piranha")));
+#endif
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApi api)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            // Initialize Piranha
+            App.Init(api);
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            // Build content types
+            new ContentTypeBuilder(api)
+                .AddAssembly(typeof(Startup).Assembly)
+                .Build()
+                .DeleteOrphans();
+
+            // Configure Tiny MCE
+            EditorConfig.FromFile("editorconfig.json");
+
+            // Middleware setup
+            app.UsePiranha(options => {
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseIdentity();
             });
         }
     }
