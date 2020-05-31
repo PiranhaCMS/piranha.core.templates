828d0931667f83ed69c764dc19fb1c0f6d43e6a7
Håkan Edling <hakan@tidyui.com>
2020-05-31 07:45:37 +0200

    Added build scripts for automatic deploy

--- pack.sh
+++ pack.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-# Define directories
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-OUTPUT_DIR=$SCRIPT_DIR/artifacts
-
 # Clean and build in release
+dotnet restore
 dotnet clean
 dotnet build -c Release
 
-# Make sure output folder exist.
-if [ ! -d "$OUTPUT_DIR" ]; then
-  mkdir "$OUTPUT_DIR"
-fi
-
 # Create all NuGet packages
-nuget pack nuspec/Piranha.Templates.nuspec -OutputDirectory $OUTPUT_DIR
+nuget pack nuspec/Piranha.Templates.nuspec -OutputDirectory ./artifacts
