2775121dfeb3c2e7e8893db5f516e86902140814
Håkan Edling <hakan@tidyui.com>
2024-07-22 20:52:34 +0200

    Updated sass compilation. Fixes #51

--- src/web/mvc/gulpfile.js
+++ src/web/mvc/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass')
+    sass = require('gulp-sass')(require('sass')),
     cssmin = require("gulp-cssmin")
     rename = require("gulp-rename");
 
